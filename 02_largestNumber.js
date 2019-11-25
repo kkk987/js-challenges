b858d63214fb92c73567a52e693d6f188f85102f
kkk987 <markying21549@gmail.com>
2019-11-25 10:27:26 +1000

    02 completed

--- 02_largestNumber.js
+++ 02_largestNumber.js
@@ -13,7 +13,19 @@ with no changes to the function?
 function largestNumber(arr) {
     // Your code here
     // Google JS syntax ;)
+    if(arr.length <= 0) return null;
+    var sortedArr = arr.sort(function (a, b) {return a-b});
+    let i = 1;
+    while(!Number.isInteger(sortedArr[sortedArr.length - i])) {
+        i++;
+    }
+    return sortedArr[sortedArr.length - i];
 }
+
+console.log(largestNumber([5, -2, 10]));
+console.log(largestNumber([10, 10, "s"]));
+console.log(largestNumber([]));
+console.log(largestNumber([-1, -2, -3]));
 
 module.exports = {
     largestNumber
